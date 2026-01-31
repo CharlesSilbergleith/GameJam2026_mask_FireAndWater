4496624b583f24dd529517d87f28a06acfcb73f0
CharlesSilbergleith <csilbergleith89567@uat.edu>
2026-01-30 20:43:39 -0700

    boss movement moves to the next area
    got the game manger and the boss to move to points saved in the game manger

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public List<Transform> bossPath = new List<Transform>();
+    public Transform boss;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void RegisterPoint(Transform point)
+    {
+        if (!bossPath.Contains(point))
+        {
+            bossPath.Add(point);
+            SortPointsByDistance();
+        }
+    }
+
+    void SortPointsByDistance()
+    {
+        if (boss == null) return;
+
+        bossPath = bossPath.OrderBy(p => Vector3.Distance(boss.position, p.position)).ToList();
+    }
+}
